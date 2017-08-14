cc6476cf1a582c6f7a8844558be5eec93377ea92
奏之章 <zouzhizhang@qq.com>
2017-08-14 10:24:37 +0800

    Add zouzhizhang's type_list

---
+++ zouzhizhang/main.cpp
@@ -0,0 +1,64 @@
+
+#include "type_list.h"
+#include <string>
+
+int main()
+{
+  using list1_t = type_list_t<int, long, char, double>;
+  using list2_t = list1_t::push_back<std::string>;
+  using list3_t = list2_t::push_front<list1_t>;
+  using list4_t = list3_t::pop_front;
+  using list5_t = list1_t::pop_front;
+  using list6_t = list5_t::pop_back;
+  using list7_t = type_list_t<int>;
+  using list8_t = list7_t::cat<list5_t>;
+  using list9_t = list1_t::slice<0, 3>;
+  using list10_t = list1_t::slice<3, 1>;
+  using list11_t = list10_t::pop_back;
+  using list12_t = list9_t::cat<list11_t>;
+  using list13_t = list11_t::cat<list9_t>;
+  using list14_t = list1_t::remove<0>;
+  using list15_t = list1_t::remove<1>;
+  using list16_t = list1_t::remove<3>;
+  using list17_t = list1_t::insert<0, float>;
+  using list18_t = list1_t::insert<1, float>;
+  using list19_t = list1_t::insert<4, float>;
+
+  static_assert(list1_t::size == 4, "no");
+  static_assert(std::is_same<list1_t::begin::type, int>::value, "no");
+  static_assert(std::is_same<list1_t::begin::next::type, long>::value, "no");
+  static_assert(std::is_same<list1_t::begin::next::next::next::next, list1_t::end>::value, "no");
+  static_assert(std::is_same<list1_t::get<0>, int>::value, "no");
+  static_assert(std::is_same<list1_t::get<1>, long>::value, "no");
+  static_assert(std::is_same<list1_t::get<2>, char>::value, "no");
+  static_assert(std::is_same<list1_t::get<3>, double>::value, "no");
+  static_assert(std::is_same<list2_t::get<4>, std::string>::value, "no");
+  static_assert(std::is_same<list3_t::get<1>, int>::value, "no");
+  static_assert(std::is_same<list3_t::get<0>, list1_t>::value, "no");
+  static_assert(std::is_same<list3_t::begin::type::begin::type, int>::value, "no");
+  static_assert(std::is_same<list2_t, list4_t>::value, "no");
+  static_assert(std::is_same<list2_t, list4_t>::value, "no");
+  static_assert(list5_t::size == 3, "no");
+  static_assert(std::is_same<list5_t::begin::type, long>::value, "no");
+  static_assert(std::is_same<list5_t::begin::next::next::next, list5_t::end>::value, "no");
+  static_assert(list6_t::size == 2, "no");
+  static_assert(std::is_same<list6_t::begin::type, long>::value, "no");
+  static_assert(std::is_same<list6_t::begin::next::next, list6_t::end>::value, "no");
+  static_assert(list8_t::size == 4, "no");
+  static_assert(std::is_same<list8_t, list1_t>::value, "no");
+  static_assert(list9_t::size == 3, "no");
+  static_assert(list10_t::size == 1, "no");
+  static_assert(list10_t::pop_back::size == 0, "no");
+  static_assert(std::is_same<list10_t, type_list_t<double>>::value, "no");
+  static_assert(std::is_same<list11_t, type_list_t<>>::value, "no");
+  static_assert(std::is_same<list12_t, list9_t>::value, "no");
+  static_assert(std::is_same<list13_t, list12_t>::value, "no");
+  static_assert(std::is_same<list14_t, type_list_t<long, char, double>>::value, "no");
+  static_assert(std::is_same<list15_t, type_list_t<int, char, double>>::value, "no");
+  static_assert(std::is_same<list16_t, type_list_t<int, long, char>>::value, "no");
+  static_assert(std::is_same<list17_t, type_list_t<float, int, long, char, double>>::value, "no");
+  static_assert(std::is_same<list18_t, type_list_t<int, float, long, char, double>>::value, "no");
+  static_assert(std::is_same<list19_t, type_list_t<int, long, char, double, float>>::value, "no");
+
+}
+
---
+++ zouzhizhang/type_list.h
@@ -0,0 +1,132 @@
+
+#include <cstddef>
+#include <type_traits>
+#include <utility>
+
+
+template<class ...args_t>
+struct type_node_t;
+template<class ...args_t>
+struct type_list_t;
+
+
+template<class T, class ...next_t>
+struct type_node_t<T, next_t...>
+{
+  using type = T;
+  using next = type_node_t<next_t...>;
+};
+template<>
+struct type_node_t<>
+{
+};
+
+template<class iter_t, size_t I>
+struct type_list_iter_move_next_t
+{
+  using iter = typename type_list_iter_move_next_t<typename iter_t::next, I - 1>::iter;
+};
+template<class iter_t>
+struct type_list_iter_move_next_t<iter_t, 0>
+{
+  using iter = iter_t;
+};
+
+template<size_t I, class list_t, class ...other_t>
+struct type_list_get_list_t;
+
+template<bool found, size_t I, class list_t, class ...other_t>
+struct type_list_get_type_t
+{
+  using iter = typename type_list_iter_move_next_t<typename list_t::begin, I>::iter;
+  using type = typename iter::type;
+};
+template<size_t I, class list_t, class ...other_t>
+struct type_list_get_type_t<false, I, list_t, other_t...>
+{
+  using type = typename type_list_get_list_t<I - list_t::size, other_t...>::type;
+};
+
+template<size_t I, class list_t, class ...other_t>
+struct type_list_get_list_t
+{
+  using type = typename type_list_get_type_t<(I < list_t::size), I, list_t, other_t...>::type;
+};
+
+template<class ...list_t>
+struct type_list_get_t
+{
+  template<size_t I>
+  struct get
+  {
+    using type = typename type_list_get_list_t<I, list_t...>::type;
+  };
+};
+
+template<class get_t, size_t B, class T>
+struct type_list_builder_t;
+template<class get_t, size_t B, size_t ...I>
+struct type_list_builder_t<get_t, B, std::integer_sequence<size_t, I...>>
+{
+  using type = type_list_t<typename get_t::template get<B + I>::type...>;
+};
+
+template<class list_t, size_t B, size_t S>
+struct type_list_slice_t
+{
+  using type = typename type_list_builder_t<type_list_get_t<list_t>, B, std::make_index_sequence<S>>::type;
+};
+
+template<class ...list_t>
+struct type_list_cat_size_t;
+template<class list_t, class ...other_list_t>
+struct type_list_cat_size_t<list_t, other_list_t...>
+{
+  static constexpr size_t size = list_t::size + type_list_cat_size_t<other_list_t...>::size;
+};
+template<>
+struct type_list_cat_size_t<>
+{
+  static constexpr size_t size = 0;
+};
+
+template<class ...list_t>
+struct type_list_cat_t
+{
+  static constexpr size_t size = type_list_cat_size_t<list_t...>::size;
+  using type = typename type_list_builder_t<type_list_get_t<list_t...>, 0, std::make_index_sequence<size>>::type;
+};
+
+template<class ...args_t>
+struct type_list_t
+{
+  using begin = type_node_t<args_t...>;
+  using end = type_node_t<>;
+  static constexpr size_t size = sizeof ...(args_t);
+  static constexpr bool empty = false;
+  template<size_t I> using get = typename type_list_get_t<type_list_t<args_t...>>::template get<I>::type;
+  using front = get<0>;
+  using back = get<size - 1>;
+  template<class T> using push_back = typename type_list_t<args_t..., T>;
+  using pop_back = typename type_list_slice_t<type_list_t<args_t...>, 0, size - 1>::type;
+  template<class T> using push_front = typename type_list_t<T, args_t...>;
+  using pop_front = typename type_list_slice_t<type_list_t<args_t...>, 1, size - 1>::type;
+  template<class other_t> using cat = typename type_list_cat_t<type_list_t<args_t...>, other_t>::type;
+  template<size_t B, size_t S> using slice = typename type_list_slice_t<type_list_t<args_t...>, B, S>::type;
+  template<size_t I, class T> using insert = typename type_list_cat_t<slice<0, I>, type_list_t<T>, slice<I, size - I>>::type;
+  template<size_t I> using remove = typename type_list_cat_t<slice<0, I>, slice<I + 1, size - I - 1>>::type;
+};
+template<>
+struct type_list_t<>
+{
+  using begin = type_node_t<>;
+  using end = type_node_t<>;
+  static constexpr size_t size = 0;
+  static constexpr bool empty = true;
+  template<class T> using push_back = type_list_t<T>;
+  template<class T> using push_front = type_list_t<T>;
+  template<class other_t> using cat = other_t;
+  template<size_t B, size_t S> using slice = typename type_list_slice_t<type_list_t<>, B, S>::type;
+  template<size_t I, class T> using insert = typename type_list_slice_t<type_list_t<T>, I, 0>::type;
+};
+
